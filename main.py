83d4656f0eb4b092c7468daec5d97365b9d408a2
LoyalWolf <94860300+LoyalWolf1404@users.noreply.github.com>
2021-12-27 22:05:34 +0100

    Updated main.py
    - Added Buttons and Userinfo
    - Updated Help

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from datetime import datetime
+from discord.enums import ButtonStyle
+from discord_components import Button, Select, SelectOption, ComponentsBot, interaction, ButtonStyle
 import discord
 from discord.ext import commands
 import discord.ext
+import pytz
+
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='!', help_command=None)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,
+                                                        name="https://github.com/LoyalWolf1404/DiscordPython",
+                                                        status=discord.Status.online))
 
 
 def caps_pls(text):
@@ -51,17 +63,44 @@ async def rechne(ctx, operation: str):
     await ctx.send(eval(operation))
 
 
+@bot.command(name='userinfo')
+async def userinfo(ctx, member: discord.Member):
+    de = pytz.timezone('Europe/Berlin')
+    embed = discord.Embed(title=f'> Userinfo für {member.display_name}',
+                          description='', color=discord.Colour.purple(), timestamp=datetime.now().astimezone(tz=de))
+
+    embed.add_field(name='Name', value=f'```{member.name}#{member.discriminator}```', inline=True)
+    embed.add_field(name='Bot', value=f'```{("Ja" if member.bot else "Nein")}```', inline=True)
+    embed.add_field(name='Nickname', value=f'```{(member.nick if member.nick else "Nicht gesetzt")}```', inline=True)
+    embed.add_field(name='Server beigetreten', value=f'```{member.joined_at}```', inline=True)
+    embed.add_field(name='Discord beigetreten', value=f'```{member.created_at}```', inline=True)
+    embed.add_field(name='Rollen', value=f'```{len(member.roles)}```', inline=True)
+    embed.add_field(name='Höchste Rolle', value=f'```{member.top_role.name}```', inline=True)
+    embed.add_field(name='Farbe', value=f'```{member.color}```', inline=True)
+    embed.add_field(name='Booster', value=f'```{("Ja" if member.premium_since else "Nein")}```', inline=True)
+    embed.set_footer(text=f'Angefordert von {ctx.author.name} • {ctx.author.id}', icon_url=ctx.author.avatar_url)
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def gh(ctx):
+    await ctx.send("_ _", components=[Button(label="Github", style=ButtonStyle.URL, url="https://github.com/LoyalWolf1404/DiscordPython")])
+
+
+
+
 @bot.command()
 async def help(context):
     embed = discord.Embed(title="Commands",
-                          url="https://github.com/LoyalWolf1404/DiscordPython",
                           description="!help - Zeigt das hier an\n\
                 !test - Einfach nur zum testen\n\
                 !kill - Bringt jemanden um jajaja.\n\
                 !say - Sagt ein word für dich, weil du zu dumm bist das wort auszusprechen\n\
                 !argumenttest - Mach einfach\n\
                 !caps - macht alles in caps\n\
-                !rechne - rechnet dir alles",
+                !rechne - rechnet dir alles\n\
+                !userinfo - zeigt dir die Userinfo an\n\
+                !gh - Github Button zu meinem Profil",
                           color=discord.Colour.purple())
     await context.send(embed=embed)
 
